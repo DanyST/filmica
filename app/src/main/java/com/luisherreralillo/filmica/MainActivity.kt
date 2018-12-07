1d6bdb03ef9180e30eab5d9cfbd42858e2e0196b
Luis Herrera Lillo <dany.herrera20_@hotmail.com>
2018-12-07 05:22:47 -0300

    Add Toast text when on click button 'Add to watch list

--- app/src/main/java/com/luisherreralillo/filmica/MainActivity.kt
+++ app/src/main/java/com/luisherreralillo/filmica/MainActivity.kt
@@ -2,11 +2,20 @@ package com.luisherreralillo.filmica
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.btn_add)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Added to list", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
