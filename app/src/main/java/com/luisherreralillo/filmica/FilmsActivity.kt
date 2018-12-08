15ffd95f3c078faa3f544a2806a38b0778f9cf1e
Luis Herrera Lillo <dany.herrera20_@hotmail.com>
2018-12-08 13:01:18 -0300

    Implement RecyclerView and sync it with model from FilmsActiviy

--- app/src/main/java/com/luisherreralillo/filmica/FilmsActivity.kt
+++ app/src/main/java/com/luisherreralillo/filmica/FilmsActivity.kt
@@ -3,6 +3,8 @@ package com.luisherreralillo.filmica
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 
 class FilmsActivity: AppCompatActivity() {
@@ -10,10 +12,17 @@ class FilmsActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_films)
+
+        val list = findViewById<RecyclerView>(R.id.list_films)
+        list.layoutManager = LinearLayoutManager(this)
+
+        val adapter = FilmsAdapter()
+        list.adapter = adapter
+        adapter.setFilms(FilmsRepo.films)
     }
 
     fun showDetails(clickedView: View) {
-        val intentToDetails: Intent = Intent(this, DetailsActivity::class.java)
+        val intentToDetails = Intent(this, DetailsActivity::class.java)
         startActivity(intentToDetails)
     }
 }
