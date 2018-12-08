041afc9f04008435ff0236f8732378bdb7c84296
Luis Herrera Lillo <dany.herrera20_@hotmail.com>
2018-12-08 03:30:57 -0300

    Add Films Repository

---
+++ app/src/main/java/com/luisherreralillo/filmica/FilmsRepo.kt
@@ -0,0 +1,24 @@
+package com.luisherreralillo.filmica
+
+// Se mantendrá en memoria como unica instancia Singleton
+object FilmsRepo {
+    val films: MutableList<Film> = mutableListOf()
+        get() {
+            // WildCard field accede a la instancia de films y no al get
+            if (field.isEmpty()) {
+                field.addAll(dummyFilms())
+            }
+        }
+
+    private fun dummyFilms(): List<Film> {
+        return (0..9).map {
+           Film(
+                title = "Film $it",
+                genre = "$it",
+                release = "200$it-0$it-0$it",
+                voteRating = it.toDouble(),
+                overview = "Overview $it"
+            )
+        }
+    }
+}
