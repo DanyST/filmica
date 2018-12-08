63db05416e3ee3b7265d2ec7b6a3a443126a2b92
Luis Herrera Lillo <dany.herrera20_@hotmail.com>
2018-12-08 00:54:53 -0300

    Add Intent to DetailsActivity from FilmsActivity

--- app/src/main/java/com/luisherreralillo/filmica/FilmsActivity.kt
+++ app/src/main/java/com/luisherreralillo/filmica/FilmsActivity.kt
@@ -1,20 +1,19 @@
 package com.luisherreralillo.filmica
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.widget.Button
+import android.view.View
 
 class FilmsActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_films)
+    }
 
-        val button: Button = findViewById(R.id.btn_film)
-        button.setOnClickListener {
-            Log.d(FilmsActivity::class.java.canonicalName, "Button was clicked")
-        }
-
+    fun showDetails(clickedView: View) {
+        val intentToDetails: Intent = Intent(this, DetailsActivity::class.java)
+        startActivity(intentToDetails)
     }
 }
