3de29626fdf0cc87728c212a448908d95adea83d
Luis Herrera Lillo <dany.herrera20_@hotmail.com>
2018-12-08 19:58:34 -0300

    Implement Fragment Films from FilmsActivity and add activity_films layout for landscape tablet

--- app/src/main/java/com/luisherreralillo/filmica/FilmsActivity.kt
+++ app/src/main/java/com/luisherreralillo/filmica/FilmsActivity.kt
@@ -1,39 +1,30 @@
 package com.luisherreralillo.filmica
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 
-class FilmsActivity: AppCompatActivity() {
-
-    val list: RecyclerView by lazy {
-        val instance = findViewById<RecyclerView>(R.id.list_films)
-        instance.layoutManager = LinearLayoutManager(this)
-
-        instance
-    }
-
-    val adapter: FilmsAdapter by lazy {
-        val instance = FilmsAdapter { film ->
-            this.showDetails(film.id)
-        }
-        instance.setFilms(FilmsRepo.films)
-
-        instance
-    }
+class FilmsActivity : AppCompatActivity(), FilmsFragment.onItemClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_films)
 
-        list.adapter = adapter
+        if (savedInstanceState == null) {
+            val filmsFragment = FilmsFragment()
+
+            // beginTransaction: Conjunto de instruccones para el manejador de fragmentos
+            supportFragmentManager.beginTransaction()
+                .add(R.id.container_list, filmsFragment)
+                .commit()
+        }
     }
 
-    fun showDetails(filmId: String) {
-        val intentToDetails = Intent(this, DetailsActivity::class.java)
-        intentToDetails.putExtra("id", filmId)
-        startActivity(intentToDetails)
+    override fun onItemClicked(film: Film) {
+        showDetails(film.id)
     }
+
+    fun showDetails(id: String) {
+
+    }
+
 }
