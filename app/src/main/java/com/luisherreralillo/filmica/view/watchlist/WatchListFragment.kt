6a5b76e8d02e06f7ea9236d1614364a3fa7060d7
Luis Herrera Lillo <dany.herrera20_@hotmail.com>
2019-02-25 23:43:17 -0300

    Add swipe handler for remove item on recyclerView from watchlist fragment

--- app/src/main/java/com/luisherreralillo/filmica/view/watchlist/WatchListFragment.kt
+++ app/src/main/java/com/luisherreralillo/filmica/view/watchlist/WatchListFragment.kt
@@ -3,10 +3,14 @@ package com.luisherreralillo.filmica.view.watchlist
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.helper.ItemTouchHelper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.luisherreralillo.filmica.R
+import com.luisherreralillo.filmica.data.FilmsRepo
+import com.luisherreralillo.filmica.view.util.SwipeToDeleteCallback
 import kotlinx.android.synthetic.main.fragment_watch_list.*
 
 class WatchListFragment : Fragment() {
@@ -27,7 +31,34 @@ class WatchListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupSwipeHandler()
         watchlist.adapter = adapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        FilmsRepo.watchlist(context!!) { films ->
+            adapter.setFilms(films.toMutableList())
+        }
+    }
+
+    private fun setupSwipeHandler() {
+        val swipeHandler = object: SwipeToDeleteCallback() {
+            override fun onSwiped(holder: RecyclerView.ViewHolder, direction: Int) {
+                deleteFilmAt(holder.adapterPosition)
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(swipeHandler)
+        itemTouchHelper.attachToRecyclerView(watchlist)
+    }
+
+    private fun deleteFilmAt(position: Int) {
+        val film = adapter.getFilm(position)
+        FilmsRepo.deleteFilm(context!!, film) {
+            adapter.removeFilmAt(position)
+        }
     }
 
 }
